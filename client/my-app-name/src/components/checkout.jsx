5cb8f9c04bda3b6083ccc8d1f024aed21edeaab6
Alani158 <mujeebalani4@gmail.com>
2025-07-17 15:21:39 +0100

    optimize 9

--- client/my-app-name/src/components/checkout.jsx
+++ client/my-app-name/src/components/checkout.jsx
@@ -17,14 +17,14 @@ export default function Checkout() {
   };
 
   return (
-    <div className="w-full mx-auto pt-10">
+    <div className="w-full mx-auto pt-0">
       <Navbar />
 
-      <div className="gap-6 justify-self-center  flex my-20   w-[90%]">
-        <div className="w-[50%] border border-black shadow-md hover:shadow-inner transition-all hover:shadow-black cursor-pointer shadow-black  rounded-lg h-96 ">
-          <h1 className="text-center text-base sm:text-3xl px-10 mt-24">
+      <div className="gap-6 sm:justify-self-center   sm:flex my-10  sm:w-[90%]">
+        <div className="sm:w-[50%] border border-black shadow-md hover:shadow-inner transition-all hover:shadow-black cursor-pointer shadow-black  rounded-lg sm:h-96 w-[80%] h-60 mx-auto ">
+          <h1 className="text-center text-base sm:text-3xl px-10 sm:mt-24 mt-5">
             {" "}
-            Sure of your order?, click on the button blow to confirm
+            Thank you for your order! Your order has been placed successfully.
           </h1>
           <div
             className="w-[50%] sm:w-[40%] mt-6 border border-black shadow-md  hover:p-2 transition-all   bg-black justify-self-center flex justify-center items-center rounded-lg cursor-pointer"
@@ -33,18 +33,6 @@ export default function Checkout() {
             <button className="text-white py-2 sm:text-2xl">
               Continue shopping
             </button>
-          </div>
-        </div>
-
-        <div className="w-[50%] border  border-black shadow-md hover:shadow-inner transition-all hover:shadow-black cursor-pointer shadow-black rounded-lg h-96 ">
-          <h2 className="text-center text-base sm:text-3xl px-10 mt-24">
-            Not sure? You can always come back to your cart and checkout anytime
-          </h2>
-          <div
-            className="w-[50%] sm:w-[40%] mt-6 border border-black shadow-md  hover:p-2 transition-all   bg-black justify-self-center flex justify-center items-center rounded-lg cursor-pointer"
-            onClick={handlebackCart}
-          >
-            <button className="text-white py-2 sm:text-2xl">Go to Cart</button>
           </div>
         </div>
       </div>
